cbc29f6cd049934adbf33f2e6e65beeffaca88ec
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2023-12-15 01:10:58 +0700

    feat: change color

---
+++ src/features/Controller/LightColor.tsx
@@ -0,0 +1,30 @@
+import Subtitle from "@/components/Subtitle";
+import { useState } from "react";
+import { debounce } from "lodash";
+import { LLLL_ACTION_PARAMS, LLLL_CHANNEL, publish } from "@/services/mqtt";
+
+export const LightColor = () => {
+    const [color, setColor] = useState<string>("#ffffff");
+
+    const handleColorChange = debounce((color: string) => {
+        setColor(color);
+        publish(
+            LLLL_CHANNEL.ACTION_CHANGE_COLOR,
+            LLLL_ACTION_PARAMS.CHANGE_COLOR(color)
+        );
+    }, 500);
+
+    return (
+        <div className="flex flex-wrap gap-2 items-center">
+            <Subtitle text="Màu sắc" />
+            <input
+                type="color"
+                value={color}
+                onChange={(e) => handleColorChange(e.target.value)}
+            />
+            <p>{color}</p>
+        </div>
+    );
+};
+
+export default LightColor;
