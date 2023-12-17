06a6b205b5204c6dbae281951a2ecfa773230e90
ltminhthu <ltminhthu.209@gmail.com>
2023-12-17 16:07:13 +0700

    adjust info

--- src/components/DeviceInfo.tsx
+++ src/components/DeviceInfo.tsx
@@ -9,11 +9,11 @@ interface DeviceInfoProps {
 
 const DeviceInfo: FC<DeviceInfoProps> = ({ id, name, onwer }) => {
   return (
-    <div>
+    <div className="flex flex-col gap-2 py-5">
       <h1 className="text-xl font-semibold text-primary">Device Information</h1>
       <div className="max-w-[600px]">
         <ItemInfo title="ID" content={id} />
-        <ItemInfo title="Name" content={name} />
+        <ItemInfo title="Name" content={name} isEditabled={true} />
         <ItemInfo title="Owner" content={onwer} />
       </div>
     </div>
--- src/components/ItemInfo.tsx
+++ src/components/ItemInfo.tsx
@@ -1,14 +1,29 @@
+import { EditOutlined } from "@ant-design/icons";
 import { FC } from "react";
 
 interface Props {
   title: string;
   content: string;
+  isEditabled?: boolean;
+  onEdit?: () => void;
 }
 
-const ItemInfo: FC<Props> = ({ title, content }) => {
+const ItemInfo: FC<Props> = ({
+  title,
+  content,
+  isEditabled = false,
+  onEdit,
+}) => {
   return (
     <div className="flex justify-between">
-      <h1 className="text-large text-tertiary font-medium">{title}</h1>
+      <div className="flex gap-1 items-center">
+        <h1 className="text-large text-tertiary font-medium">{title}</h1>
+        {isEditabled && (
+          <button className="flex items-center" onClick={onEdit}>
+            <EditOutlined className="text-primary cursor-pointer" />
+          </button>
+        )}
+      </div>
       <p className="text-large">{content}</p>
     </div>
   );
