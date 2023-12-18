c7db8273b79d4d2cd51f86f8c11c95edbeaac12a
ltminhthu <ltminhthu.209@gmail.com>
2023-12-18 16:34:46 +0700

    update name of device

--- src/components/DeviceInfo.tsx
+++ src/components/DeviceInfo.tsx
@@ -1,29 +1,33 @@
 import { FC } from "react";
 import ItemInfo from "./ItemInfo";
+import { updateDeviceName } from "@/services/firebase";
 
 interface DeviceInfoProps {
-    id: string;
-    name: string;
-    owner: string;
+  id: string;
+  name: string;
+  owner: string;
 }
 
 const DeviceInfo: FC<DeviceInfoProps> = ({ id, name, owner }) => {
-    return (
-        <div className="flex flex-col gap-2 py-5">
-            <h1 className="text-xl font-semibold text-primary">
-                Thông tin thiết bị
-            </h1>
-            <div className="max-w-[600px]">
-                <ItemInfo title="Mã thiết bị" content={id} />
-                <ItemInfo
-                    title="Tên thiết bị"
-                    content={name}
-                    isEditabled={true}
-                />
-                <ItemInfo title="Chủ thiết bị" content={owner} />
-            </div>
-        </div>
-    );
+  const handleEditName = async (content: string) => {
+    await updateDeviceName(id, content);
+  };
+
+  return (
+    <div className="flex flex-col gap-2 py-5">
+      <h1 className="text-xl font-semibold text-primary">Thông tin thiết bị</h1>
+      <div className="max-w-[600px]">
+        <ItemInfo title="Mã thiết bị" content={id} />
+        <ItemInfo
+          title="Tên thiết bị"
+          content={name}
+          isEditabled={true}
+          onEdit={handleEditName}
+        />
+        <ItemInfo title="Chủ thiết bị" content={owner} />
+      </div>
+    </div>
+  );
 };
 
 export default DeviceInfo;
--- src/components/ItemInfo.tsx
+++ src/components/ItemInfo.tsx
@@ -2,61 +2,59 @@ import { EditOutlined } from "@ant-design/icons";
 import { FC, useState } from "react";
 
 interface Props {
-    title: string;
-    content: string;
-    isEditabled?: boolean;
-    onEdit?: (content: string) => void;
+  title: string;
+  content: string;
+  isEditabled?: boolean;
+  onEdit?: (content: string) => void;
 }
 
 const ItemInfo: FC<Props> = ({
-    title,
-    content,
-    isEditabled = false,
-    onEdit,
+  title,
+  content,
+  isEditabled = false,
+  onEdit,
 }) => {
-    const [editedContent, setEditedContent] = useState(content);
-    const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(content);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === "Enter") {
-            setIsEditing(false);
-        }
-    };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      setIsEditing(false);
+    }
+    if (onEdit) {
+      onEdit(editedContent);
+    }
+  };
 
-    return (
-        <div className="flex justify-between">
-            <div className="flex gap-1 items-center">
-                <h1 className="text-large text-tertiary font-medium">
-                    {title}
-                </h1>
+  return (
+    <div className="flex justify-between">
+      <div className="flex gap-1 items-center">
+        <h1 className="text-large text-tertiary font-medium">{title}</h1>
 
-                {isEditabled && (
-                    <button
-                        className="flex items-center"
-                        onClick={() => {
-                            setIsEditing(!isEditing);
-                            if (onEdit) {
-                                onEdit(editedContent);
-                            }
-                        }}
-                    >
-                        <EditOutlined className="text-primary cursor-pointer" />
-                    </button>
-                )}
-            </div>
+        {isEditabled && (
+          <button
+            className="flex items-center"
+            onClick={() => {
+              setIsEditing(!isEditing);
+            }}
+          >
+            <EditOutlined className="text-primary cursor-pointer" />
+          </button>
+        )}
+      </div>
 
-            {isEditing ? (
-                <input
-                    type="text"
-                    value={editedContent}
-                    onChange={(text) => setEditedContent(text.target.value)}
-                    onKeyDown={(event) => handleKeyDown(event)}
-                />
-            ) : (
-                <p className="text-large">{editedContent}</p>
-            )}
-        </div>
-    );
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedContent}
+          onChange={(text) => setEditedContent(text.target.value)}
+          onKeyDown={(event) => handleKeyDown(event)}
+        />
+      ) : (
+        <p className="text-large">{editedContent}</p>
+      )}
+    </div>
+  );
 };
 
 export default ItemInfo;
